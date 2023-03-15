42e2d17bbef3c3bfb0deebc78ad332efd2559be6
unknown <tulio.correia.tcr@gmail.com>
2023-03-15 16:42:28 -0300

    aula 2

--- Console_log.js
+++
@@ -1,2 +0,0 @@
-// Primeiro console.log
-console.log('Meu nome é "Túlio". Estou aprendendo JavaScript às', 10, 'da manhã.');
---
+++ first_steps/Console_log.js
@@ -0,0 +1,4 @@
+// Primeiro console.log
+console.log('Meu nome é "Túlio". Estou aprendendo JavaScript às', 10, 'da manhã.');
+
+/* teste */
